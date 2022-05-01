d49fec8d66b2515d42856289a11e21bf60b9de7a
Emilio Vargas Millan <emiliovm@outlook.es>
2022-05-01 18:22:03 +0200

    hasta el ejercicio 19

--- javascript.js
+++ javascript.js
@@ -138,3 +138,81 @@ let var1 = "Hey Tú, cómo te llamas. ¡Hey, estoy hablando contigo!";
 
 console.log (var1);
 console.log (var1.replace("contigo", "yo"));
+
+/* Ejercicio de clase: escribir un programa que pida un numero al usuario y devuelva "java" si el numero es multiplo de 3, devuelva "script" si es multiplo de 5, devuelva "javascript" si es multiplo de 7, en caso contrario devuelva "W bootstrap 4?" */
+
+/* let multiplos = prompt ("introduce el número para saber si es múltiplo");
+
+if (multiplos % 3 == 0) {
+    console.log  ("es múltiplo de 3")
+}
+else if (multiplos % 5 == 0) {
+    console.log ("script")
+}
+else if (multiplos % 7 == 0) {
+    console.log ("javascript")
+}
+else{
+    console.log ("W bootstrap 4 🚀")
+} */
+
+// 15 Crear un programa que dada una variable number nos diga si es mayor que 100.
+
+let numberr = 101;
+
+if (numberr >= 100) {
+    console.log ("la variable es mayor que 100")
+} else{
+    console.log ("la variable es menor que 100")
+}
+
+// 16 Crear un programa que dada una variable number nos diga si es positivo o negativo.
+
+const numerito = 1;
+
+if (numerito > 0) {
+    console.log ("positivo")
+} else {
+    console.log ("negativo")
+}
+
+// 17 Crear un programa que dadas 2 variables tipo number, las sume. Si la suma es mayor que 50 multiplique por 2, si no, por 3.
+
+let variable1 = 30;
+let variable2 = 20;
+
+const sumavar = (variable1 + variable2)
+
+if (sumavar > 50) {
+    console.log (sumavar * 2)
+} else {
+    console.log (sumavar * 3)
+}
+
+// 18 Crear un programa que dada una variable number, nos diga si es un múltiplo de 3. Si no lo es, nos diga si es múltiplo de 8 y, si no, imprima un mensaje de error.
+
+const diezyocho = 17;
+
+if (diezyocho % 3 == 0) {
+    console.log (`${diezyocho} es múltiplo de 3`)
+}
+else if (diezyocho % 8 == 0) {
+    console.log (`${diezyocho} es múltiplo de 8`)
+}
+else {
+    console.log ("Fatal error 404 not found")
+}
+
+// 19 Crear un programa que dadas 2 variables booleanas, “buen_tiempo” y “trabajo”, nos diga si puedo ir a la playa. Si trabajo, no puedo ir, si no trabajo verifico que haga buen tiempo. En los casos negativos imprimir un mensaje adecuado.
+
+const buen_tiempo = false;
+const trabajo = false;
+
+if (trabajo) {
+    console.log ("no puedes ir porque trabajas, cara pan")
+} else if (!trabajo && buen_tiempo) {
+    console.log ("Puedes ir a la plallita, ya que no trabajas y hace un tiempo maravilloso")
+}
+else {
+    console.log ("no puedes ir, parece que hará mal tiempo, sorry")
+}
